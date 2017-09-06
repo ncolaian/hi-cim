f4a04b52470217d0d8afc8597fa4016256039e62
Phanstiel 4 <phanstiel4@wire152019098159.med.unc.edu>
2017-09-06 13:28:37 -0400

    opts to opt

--- data_analysis/get_feature_specific_data.R
+++ data_analysis/get_feature_specific_data.R
@@ -91,7 +91,7 @@ print_out_data <- function(name, dataframe, chr, out) {
 
 #### MAIN ####
 separate_loops_and_tads(opts$chromosome, loop_df, read_counts)
-print_out_data("loop_flare_", flares_and_loops_dvc, opts$chromosome, opts$out_dir)
-print_out_data("background_", background_counts, opts$chromosome, opts$out_dir)
-print_out_data("TADs_", dist_vs_avg_counts, opts$chromosome, opts$out_dir)
+print_out_data("loop_flare_", flares_and_loops_dvc, opt$chromosome, opt$out_dir)
+print_out_data("background_", background_counts, opt$chromosome, opt$out_dir)
+print_out_data("TADs_", dist_vs_avg_counts, opt$chromosome, opt$out_dir)
 
