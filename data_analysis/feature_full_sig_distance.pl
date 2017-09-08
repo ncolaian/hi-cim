1b20657ac716ef1916c4958d914411a3ff1213ab
Phanstiel 4 <phanstiel4@wire152019098159.med.unc.edu>
2017-09-08 12:28:12 -0400

    Fixed the stats problem

---
+++ data_analysis/feature_full_sig_distance.pl
@@ -0,0 +1,159 @@
+#! /bash/usr/env perl
+
+# Will create a combined matrice and combined distance to signal graph. Assumes the data is from a human
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Pod::Usage;
+use Carp;
+use Path::Class;
+#use Log::Log4Perl qw(easy);
+#use Log::Log4perl::CommandLine qw(:all);
+#use Data::Dumper;
+
+#My variables
+my $help = 0;
+my $man = 0;
+my $base_name;
+my $out;
+my $loop_file;
+my $bin_length;
+my $rbin;
+
+# Create Chrom Array
+my @chrom;
+$chrom[23] = "Y";
+$chrom[22] = "X";
+for ( my $i = 0; $i < scalar(@chrom); $i++ ) {
+	$chrom[$i] = $i + 1;
+}
+
+
+#Read in the variables from the command line
+GetOptions( 'man'		=>	\$man,
+			'help|h'	=>	\$help,
+			'base_file_name|bf=s'	=>	\$base_name,
+			'out_dir|o=s'	=>	\$out,
+			'loop_file|l=s'	=>	\$loop_file,
+			'bin_length|bl=i'	=> \$bin_length,
+			'rcode_bin|r=s'	=>	\$rbin,
+		   ) || die("There was an error in the command line arguements");
+
+# Pod Usage for the manual and the help pages
+if ($help) { pod2usage(0) }
+if ($man) { pod2usage(-verbose => 3) }
+
+# Setup Logging Environment
+my $logger = get_logger();
+
+## MAIN ##
+
+
+## Subroutines ##
+sub get_count_file_names_for_each_chrom {
+	my ( $chrom_aref, $bn ) = @_;
+	$logger->info("Geting the count files for each chromosome");
+	my @dir_full = split( /\//, $bn);
+	my $just_name = pop(@dir_full);
+	my $dir = join("", @dir_full);
+	
+	#fill a hash with file names
+	my %file;
+	foreach my $chr ( @$chrom_aref ) {
+		opendir (my $DIR, $dir);
+		
+		while ( readdir $DIR ) {
+			next if ( $_ =~ /^\./ );
+			if ( $_ =~ qr/$just_name/ && $_ =~ qr/$bin_length\Kb/ && $_ =~ qr/chr$chr/ ) {
+				$file{$chr} = "$dir/$_";
+			}
+		}
+	}
+	#return hash with count file names
+	return \%file;
+}
+
+sub create_matrice_files {
+	my ( $count_href, $o ) = @_;
+	my @jobs;
+	foreach my $key ( keys %$count_href ) {
+		my $file = $count_href->{$key};
+		my $cmd = "Rscript $rbin/get_feature_specific_data.R --loop_file $loop_file --count_file $file --chromosome $key --out_dir $o";
+		push @jobs, $cmd;
+	}
+	submit_and_stall(\@jobs, "matrice", $o, "20", "12:00:00");
+	
+	return;
+}
+
+sub merge_matrice_and_create_dist_vs_sig_graph {
+	my ( )
+}
+
+sub submit_and_stall {
+	my ( $job_aref, $job_name, $out_directory, $mem, $time) = @_;
+	$logger->info("Submitting and Stalling until all $job_name jobs finish\n");
+	
+	foreach my $cmd (@$job_aref) {
+		my $bsub_cmd = "bsub -M $mem -J $job_name -o $out_directory/$job_name.out -t $time $cmd";
+		system($bsub_cmd);
+	}
+	
+	#Create Active Jobs File
+	`echo start > $out_directory/ACTIVE_JOBS`;
+	
+	my $minutes = 0;
+	while ( -s "$out_directory/ACTIVE_JOBS" ) {
+		sleep 60;
+		my $cmd = "bjobs -J " . $job_name . " > $out_directory/ACTIVE_JOBS";
+		`$cmd`;
+		$minutes++;
+	}
+	
+	`rm $out_directory/ACTIVE_JOBS`;
+	$logger->info("All $job_name jobs have finished in ~$minutes minutes");
+	
+	return 1;
+}
+
+__END__
+=head1	TITLE
+
+=head1	VERSION
+
+=head1	INCLUDED MODULES
+
+Getopt::Long;
+Pod::Usage;
+Carp;
+Path::Class;
+Log::Log4Perl qw(easy);
+Log::Log4perl::CommandLine qw(:all);
+Data::Dumper;
+
+=head1	SYNOPSIS
+
+=head1	PARAMETERS
+
+=head1	CONFIGURATION AND ENVIRONMENT
+
+=head1	DEPENDENCIES
+
+=head1	INCOMPATIBILITIES
+
+	None reported.
+
+=head1	BUGS AND LIMITATIONS
+
+	Please report any bugs or feature requests
+	
+=head1	AUTHOR
+
+Nicholas Colaianni
+Contact via email C<< <ncolaian@live.unc.edu> >>
+
+=head1	LICENCE AND COPYRIGHT
+
+=head1	DISCLAIMER OF WARRANTY
--- data_analysis/graph_tot_dist_sig.R
+++ data_analysis/graph_tot_dist_sig.R
@@ -7,60 +7,76 @@ args = commandArgs(trailingOnly=TRUE) # What you need to pass in arguements
 library(ggplot2)
 library(plyr)
 
+#Test
+#args <- vector(length = 7)
+#args[2] <- "/Users/phanstiel4/Documents/code_rep/data/background_20.txt"
+#args[3] <- "/Users/phanstiel4/Documents/code_rep/data/TADs_20.txt"
+#args[4] <- "/Users/phanstiel4/Documents/code_rep/data/loop_flare_20.txt"
+#args[5] <- "/Users/phanstiel4/Documents/code_rep/data/background_3.txt"
+#args[6] <- "/Users/phanstiel4/Documents/code_rep/data/TADs_3.txt"
+#args[7] <- "/Users/phanstiel4/Documents/code_rep/data/loop_flare_3.txt"
+
+
 #full data
-full_background <- matrix(ncol=4)
-full_background <- as.data.frame(full_background)
-colnames(full_background) <- c("distance", "means", "sd", "N")
+full_background <- read.delim(args[2], sep = "\t", header = TRUE)
 
 #TADS
-tad <- matrix(ncol=4)
-tad <- as.data.frame(tad)
-colnames(tad) <- c("distance", "means", "sd", "N")
+tad <- read.delim(args[3], sep = "\t", header = TRUE)
 
 #Loops and flares
-landf <- matrix(ncol=4)
-landf <- as.data.frame(landf)
-colnames(landf) <- c("distance", "means","sd", "N")
+landf <- read.delim(args[4], sep = "\t", header = TRUE)
 
 #### Function to read in files ####
-pooled_stats <- function(data) {
+pooled_stats <- function(data_df) {
   new_df <- matrix(ncol=4)
   new_df <- as.data.frame(new_df)
   colnames(new_df) <- c("distance", "means","sd", "N")
   
-  data$sd[is.na(data$sd)] <- 0
-  data$sd <- data$sd^2
-  for ( i in data$distance ) {
+  data_df$sd[is.na(data_df$sd)] <- 0
+  data_df$sd <- data_df$sd^2
+  for ( i in data_df$distance ) {
+    if ( is.na(i) ) next
+    if ( i >= 251 ) break
     if ( !(i %in% new_df$distance) ) {
-      distance_portion <- data[i %in% data$distance,]
-      total_N <- sum(data_portion$N-1)
-      t <- sum(data_portion$N)
-      m <- sum(data_portion$means * ((data_portion$N-1)/total_N))
-      v <- sum(data_portion$sd * ((data_portion$N-2)/total_N))
-      sd <- sqrt(v)
-      mm <- data.frame(i,m,sd,t)
+      data_portion <- data_df[data_df$distance == i,]
+      data_portion <- na.omit(data_portion)
+      if( nrow(data_portion) == 1 ) {
+        mm <- data.frame(i,data_portion$means[1], data_portion$sd[1],data_portion$N[1])
+      }
+      else {
+        total_N <- sum(data_portion$N-1)
+        t <- sum(data_portion$N)
+        m <- sum(data_portion$means * ((data_portion$N-1)/total_N))
+        v <- sum(data_portion$sd * ((data_portion$N-1)/total_N))
+        sd <- sqrt(v)
+        mm <- data.frame(i,m,sd,t)
+      }
       colnames(mm) <- colnames(new_df)
-      rbind(new_df, mm)
+      new_df <- rbind(new_df, mm)
     }
+    
   }
-  
-  return(new_df)
+  return(na.omit(new_df))
 }
 
 read_in_and_add <- function(file, df) {
   add_df <- read.delim(file, sep = "\t", header = TRUE)
   df <- rbind(df, add_df)
+  df <- pooled_stats(df)
+  df[is.nan(df$sd)] <- 0
+  print(df)
+  return(df)
 }
 
-
+#Need to fix pooled stats
 
 
 ### MAIN ###
 #Read in the files 3 at a time. The files should be submitted by chromosome in order of full tad and flares
-for ( i in seq(2,3,length(args)) ) {
-  read_in_and_add(args[i], full_background)
-  read_in_and_add(args[i+1], tad)
-  read_in_and_add(args[i+2], landf)
+for ( i in seq(5,length(args), by = 3) ) {
+  full_background <- read_in_and_add(args[i], full_background)
+  tad <- read_in_and_add(args[i+1], tad)
+  landf <- read_in_and_add(args[i+2], landf)
 }
 
 #create pooled stats
