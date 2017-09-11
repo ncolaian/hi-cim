f4c6a291c363819bb90f1e5240d3c6f07953fee2
Phanstiel 4 <phanstiel4@wire152019098159.med.unc.edu>
2017-09-11 09:55:31 -0400

    fixed bug

--- data_analysis/feature_full_sig_distance.pl
+++ data_analysis/feature_full_sig_distance.pl
@@ -75,7 +75,7 @@ sub get_count_file_names_for_each_chrom {
 	foreach my $chromo ( @$chrom_aref ) {
 		foreach my $fname ( @full_dir_files ) {
 			next if ( $fname =~ /^\./ );
-			print "$fname\t$bin_length\t$chromo\n";
+			print "$fname\t$bin_length\t$chromo\t$just_name\n";
 			if ( $fname =~ qr/$bin_length\Kb/ && $fname =~ qr/chr$chromo/ ) {
 				$file{$chromo} = "$dir/$fname";
 				print "made it\n";
