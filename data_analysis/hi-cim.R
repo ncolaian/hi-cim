c3da0da0fa4cda660a86602e900dfb5a97912127
Phanstiel 4 <phanstiel4@wire152019098159.med.unc.edu>
2017-09-25 14:30:01 -0400

    hi-cim fisrt graph

--- data_analysis/hi-cim.R
+++ data_analysis/hi-cim.R
@@ -4,7 +4,7 @@ args = commandArgs(trailingOnly=TRUE) # What you need to pass in arguements
 
 library(getopt)
 library(Sushi,lib.loc="/nas02/home/n/c/ncolaian/R/x86_64-pc-linux-gnu-library/3.2")
-#library(Sushi) for testing
+#library(Sushi)# for testing
 
 #Handles input and puts the options passed into the program into a matrix
 #Matrix makes it easier to look up values.
@@ -19,14 +19,14 @@ params = matrix(c(
 opt = getopt(params)
 
 #test
-#opt <- c("l", "a", 1, 1, 1, 1)
-#names(opt) <- c("loop_file","distr_data", "chrom", "start", "end", "bin_length")
-#opt$loop_file <- "/Users/phanstiel4/Documents/code_rep/data/CI_THP1_O_0.0.0.loops.10Kb.bedpe"
-#opt$distr_data <- "/Users/phanstiel4/Documents/sim_graphs/distribution/distr_50.txt"
-#opt$chrom <- 20
-#opt$start <- 1600000
-#opt$end <- 10000000
-#opt$bin_length <- 10
+opt <- c("l", "a", 1, 1, 1, 1)
+names(opt) <- c("loop_file","distr_data", "chrom", "start", "end", "bin_length")
+opt$loop_file <- "/Users/phanstiel4/Documents/code_rep/data/CI_THP1_O_0.0.0.loops.10Kb.bedpe"
+opt$distr_data <- "/Users/phanstiel4/Documents/sim_graphs/distribution/distr_50.txt"
+opt$chrom <- 20
+opt$start <- 1600000
+opt$end <- 4000000
+opt$bin_length <- 10
 
 
 
@@ -43,7 +43,7 @@ create_sim_matrix <- function(starting, ending, bl) {
 }
 
 get_loop_data <- function(starting, ending, loop, chr) {
-  needed_vals <- loop[loop$bbin_start <= ending & loop$abin_start >= starting & as.integer(loop$achr) == chr,] # 
+  needed_vals <- loop[loop$bbin_start <= ending & loop$abin_start >= starting & as.character(loop$achr) == paste("chr",chr,sep=""),] #  
   return(needed_vals)
 }
 
@@ -52,13 +52,13 @@ get_areas_in_tads_flares_loops <- function(need_loop, bl) {
   flare_vec <- c()
   tad_vec <- c()
 
-  for(i in length(need_loop)) {
+  for(i in seq(1:length(need_loop$abin_start))) {
     starting <- need_loop$abin_start[i]
     ending <- need_loop$bbin_start[i]
     
-    loop_vec <- c(loop_vec, paste(starting, ":", ending, sep = ""))
-    flare_vec <- c(flare_vec, get_flares(starting, ending, bl))
-    tad_vec <- c(tad_vec, get_tads(starting,ending, bl))
+    loop_vec <- append(loop_vec, paste(starting, ":", ending, sep = ""))
+    flare_vec <- append(flare_vec, get_flares(starting, ending, bl))
+    tad_vec <- append(tad_vec, get_tads(starting,ending, bl))
   }
   
   return(list(loop_vec, flare_vec, tad_vec))
@@ -110,13 +110,13 @@ add_value <- function(x,y, full_matrix, distr, vec_list, bl) {
   num_loops <- sum((vec_list[[1]] == paste(x,":", y, sep = "")), na.rm = TRUE )
   num_flares <- sum((vec_list[[2]] == paste(x,":", y, sep = "")), na.rm = TRUE)
   num_tads <- sum((vec_list[[3]] == paste(x,":", y, sep = "")), na.rm = TRUE )
-  
-  dist <- (y-x)/bl
+
+  dist <- abs((y-x)/bl)
   if(dist>30){
-    dist = 30
+    dist <- 30
   }
   if(dist==0){
-    dist = 1
+    dist <- 1
   }
     
   value <- 0;
@@ -136,7 +136,6 @@ add_value <- function(x,y, full_matrix, distr, vec_list, bl) {
   }
   
   #this means it is a background pixel
-  
   if(value == 0) {
     val_vec <- rgamma(1, as.numeric(distr$scale[distr$distance == dist & distr$model == "Background"]), rate = as.numeric(distr$rate[distr$distance == dist & distr$model == "Background"]))
     value <- sum(val_vec)
@@ -144,15 +143,18 @@ add_value <- function(x,y, full_matrix, distr, vec_list, bl) {
   if(is.na(value)){
     value <- 0
   }
-  full_matrix[paste(x,"lab",sep="_"),paste(y,"lab",sep="_")] <- value
-  return(full_matrix)
+  print(value)
+  return(value)
 }
 
-create_hic_heatmap <- function(matrix, chr, starting, ending, zr=c(0,200), pal=pal_O,
-                               legend=T, lab="Simulated Data", res=hicres) {
+create_hic_heatmap <- function(matrix, chr, starting, ending, bl, zr=c(0,100),
+                               legend=T, lab="Simulated Data") {
   chromo <- paste("chr", chr, sep="")
+  vec <- seq(starting, ending, by = bl)
+  colnames(matrix) <- vec
+  rownames(matrix) <- vec
   
-  hcplot <- plotHic(matrix, chromo, starting, ending, zrange=zr, palette = pal, resolution=res)
+  hcplot <- plotHic(matrix, chromo, starting, ending, zrange=zr, palette = colorRampPalette(c("white","dodgerblue2")))
   labelgenome(chrom=chromo,starting, ending, scale="Mb")
   title(main="Simulated Hi-C Data")
   
@@ -184,11 +186,14 @@ loops_to_sim <- get_loop_data(opt$start, opt$end, loop, opt$chrom)
 non_bg <- get_areas_in_tads_flares_loops(loops_to_sim, opt$bin_length)
 
 #go through and add values to the sim matrix
+for(i in rownames(sim_mat)) {
+  sim_mat[i,] <- sapply(colnames(sim_mat), function(x) { add_value(as.integer(gsub( "_lab",replacement = "", x)), as.integer(gsub("_lab",replacement = "",i)), sim_mat, distr_data, non_bg, opt$bin_length)} )
+}
 
-new_sim_mat <- sapply( rownames(sim_mat), function(x) sapply(colnames(sim_mat), function(y) { 
-    add_value(as.integer(gsub("_lab",replacement = "",x)),as.integer(gsub("_lab",replacement = "" ,y)), sim_mat, distr_data, non_bg, opt$bin_length) }))
+#new_sim_mat <- sapply( rownames(sim_mat), function(x) sapply(colnames(sim_mat), function(y) { 
+    #add_value(as.integer(gsub("_lab",replacement = "",x)),as.integer(gsub("_lab",replacement = "" ,y)), sim_mat, distr_data, non_bg, opt$bin_length) }))
 
-hicp <- create_hic_heatmap(sim_mat, opt$chrom, opt$start, opt$end)
+hicp <- create_hic_heatmap(sim_mat, opt$chrom, opt$start, opt$end, opt$bin_length)
 
 png("sim_hic.png")
 print(hicp)
